4c16a2cbab6cfd2be9f6fbdb366c622c81c37f9f
carpmatias <cardenasmatias.1990@gmail.com>
2021-05-25 12:14:53 +0200

    - Adding admin user tests
    Updating admin.py to customize our admin user

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,27 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email="admin@learning.com",
+            password="test123"
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email="test@learning.com",
+            password="test123",
+            name="Regular User Name Sample"
+        )
+
+    def test_users_listed(self):
+        """ Tests that users are listed on user page """
+        # These urls are available in django admin documentation
+        url = reverse("admin:core_user_changelist")
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)

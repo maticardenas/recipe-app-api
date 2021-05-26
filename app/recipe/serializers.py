00b2425ad5af7aa98b16972019e2086ffae81e0e
carpmatias <cardenasmatias.1990@gmail.com>
2021-05-26 15:11:54 +0200

    Adding list recipes endpoint and feature
    Adding tests

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from core.models import Tag, Ingredient
+from core.models import Tag, Ingredient, Recipe
 
 class TagSerializer(serializers.ModelSerializer):
     """ Serializer for tag objects """
@@ -17,4 +17,24 @@ class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ingredient
         fields = ("id", "name")
+        read_only_fields = ("id",)
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """ Serializer for recipe objects """
+    # For foreign keys they need to be defined as special fields
+    # We only want to list the ids of the ingredients, that's why we use PrimaryKeyRelatedField
+    ingredients = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Ingredient.objects.all()
+    )
+    tags = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Tag.objects.all()
+    )
+
+
+    class Meta:
+        model = Recipe
+        fields = ("id", "title", "ingredients", "tags", "time_minutes", "price", "link")
         read_only_fields = ("id",)

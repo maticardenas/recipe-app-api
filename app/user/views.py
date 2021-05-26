f01869d0f177f667d79e27c44bfe42c4a17082a4
carpmatias <cardenasmatias.1990@gmail.com>
2021-05-26 20:34:50 +0200

    Adding Recipe Image Upload feature
    Adding tests

--- app/user/views.py
+++ app/user/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
+
 class CreateUserView(generics.CreateAPIView):
     """ Create a new user in the system """
     serializer_class = UserSerializer
